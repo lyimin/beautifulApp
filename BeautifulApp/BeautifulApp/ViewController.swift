d28c535a9f88c676d87a5f9d4f74a7ed35c8dbc0
liangyiming <1142343535@qq.com>
2015-11-07 11:08:56 +0800

    完成图标和app加载图片的配置

--- BeautifulApp/BeautifulApp/ViewController.swift
+++ BeautifulApp/BeautifulApp/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
